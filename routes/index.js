4e122053fd7b5405eb7aff26add3fc82d4b00d6d
Stan004 <flywwww004@gmail.com>
2018-11-01 02:15:19 +0800

    daybook route test and show on index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  models.DayBook.findAll().then(function(daybooks) {
+    res.render('index', {
+      title: 'daybook test',
+      daybooks: daybooks,
+    });
+  });
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
